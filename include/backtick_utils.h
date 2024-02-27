b77bc993bcd61071669f8d47bd2056c268d5dbf0
smoowe2125 <smoowe2125@o2.pl>
2024-02-27 16:34:29 +0100

    Tokenizer implementation which supports single line comments, floating point numbers and doesn't leak any memory and repair some messy code.

--- include/backtick_utils.h
+++ include/backtick_utils.h
@@ -2,5 +2,22 @@
 
 #include <stdlib.h>
 
-// macro for reporting errors during scanning / tokenization
-#define SCANNING_ERROR(...) {fprintf(stderr, __VA_ARGS__); exit(1);}
+#include <tokenizer.h>
+
+#define SCAN_ERROR 1
+#define PARSE_ERROR 2
+
+// macro for reporting errors
+#define REPORT_ERROR(err_type, ...) {\
+    fprintf(stderr,  "[Line %d]", current_line); \
+    switch(err_type) \
+    { \
+        case SCAN_ERROR: \
+            fprintf(stderr, "Scanning error: "); \
+            break; \
+        case PARSE_ERROR: \
+            fprintf(stderr, "Parsing error: "); \
+            break; \
+    } \
+    fprintf(stderr, __VA_ARGS__); exit(1); \
+}
